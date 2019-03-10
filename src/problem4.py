5e7a6d5e3fdad5b74aa74fb391c2d9faecf442b0
Vidya <vidya.ga@gmail.com>
2019-03-10 17:53:08 +0530

    Removed assumption and increased range from 899 to 99 (decreasing order)

--- src/problem4.py
+++ src/problem4.py
@@ -18,11 +18,6 @@ Find the largest palindrome made from the product of two 3-digit numbers.
 
 >>>>>>>>>>>
 
-Assumptions  / Limitations : 
-    
-1. The largest palindrome will be a product between the numbers 900 - 1000
-
-
 Result : 
 Expected Answer - 906609 = 913*993
 
@@ -35,9 +30,9 @@ def isPalindrome(snumber):
 
 max_palindrome_multiple = 0
 
-for x in range(999, 899, -1):
+for x in range(999, 99, -1):
     # To avoid double counting - start at x
-    for y in range(x, 899, -1):
+    for y in range(x, 99, -1):
         multiple = x * y
 
         if isPalindrome(str(multiple)):
@@ -51,3 +46,4 @@ print('Largest Palindrome of product of two 3-digit numbers ',
       max_palindrome_multiple)
 print('x ', x_at_max_palindrome)
 print('y ', y_at_max_palindrome)
+
