9653d88106d67f238f3b8ab8acad511066393e66
Vidya <vidya.ga@gmail.com>
2019-06-29 10:20:22 +0530

    Solution to Project Euler Problem 14

---
+++ src/problem14.py
@@ -0,0 +1,82 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Jun 29 10:11:02 2019
+
+@author: Vidya
+
+Solution to Project Euler problem 14
+https://projecteuler.net/problem=14
+
+Problem Statement :
+    
+>>>>>>>>>>>  Longest Collatz sequence
+
+The following iterative sequence is defined for the set of positive integers:
+
+n → n/2 (n is even)
+n → 3n + 1 (n is odd)
+
+Using the rule above and starting with 13, we generate the following sequence:
+13 → 40 → 20 → 10 → 5 → 16 → 8 → 4 → 2 → 1
+
+It can be seen that this sequence (starting at 13 and finishing at 1) contains 
+10 terms. Although it has not been proved yet (Collatz Problem), it is 
+thought that all starting numbers finish at 1.
+
+Which starting number, under one million, produces the longest chain?
+
+NOTE: Once the chain starts the terms are allowed to go above one million.
+    
+Result : 
+
+Expected Answer - 837799
+
+"""
+
+import time
+
+collatz_seq = {1: 1}
+
+
+def colatz_number(number):
+
+    if number in collatz_seq.keys():
+        return collatz_seq[number]
+    else:
+
+        if number % 2 == 0:
+            collatz_num = int(number / 2)
+        else:
+            collatz_num = int((3 * number) + 1)
+
+        collatz_seq[number] = collatz_num
+    return collatz_num
+
+
+if __name__ == '__main__':
+
+    start_time = time.time()
+
+    max_limit = 1000000
+
+    max_collatz_number = 1
+    max_counter = 0
+
+    for i in range(1, max_limit):
+
+        col_next_num = i
+        counter = 1
+
+        while col_next_num > 1:
+            col_next_num = colatz_number(col_next_num)
+            counter += 1
+
+        if counter > max_counter:
+            max_counter = counter
+            #print('\nMax counter is ', max_counter)
+            max_collatz_number = i
+
+    total_time = time.time() - start_time
+    print(
+        f'\nThe number with the longest chain below {max_limit} is {max_collatz_number} with its counter as {max_counter}')
+    print('Time taken ', total_time)
